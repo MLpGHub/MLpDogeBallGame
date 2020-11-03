75d0dea8d6f7d80706f0eb84b7d0e0b6f2bdd827
MLpGHub <michael.leopold@mlphomeuse.de>
2020-11-03 17:10:33 +0100

    Add moving Ball controlled by Arrow Keys

---
+++ scripts/Ball.gd
@@ -0,0 +1,30 @@
+extends KinematicBody
+
+#var playerName ="MLpGolfBall"
+var coinCounter
+var velocity = Vector3(0,0,0)
+const SPEED = 10
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):	
+		velocity.x = -SPEED
+	else:
+		velocity.x = lerp(velocity.x,0,0.1)
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):	
+		velocity.z = SPEED
+	else:
+		velocity.z = lerp(velocity.z,0,0.1)
+	#TODO Gravity is within the (velocity) by using (velocity, ... ) 
+	move_and_slide(velocity)
+	
